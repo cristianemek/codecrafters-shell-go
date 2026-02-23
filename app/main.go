688f5b0a6f5a2d93a45b8f6bac5d0f258c43a4d8
Cristianemek <cristianrodriguezlugo@gmail.com>
2026-02-23 23:10:48 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -8,30 +8,53 @@ import (
 )
 
 func main() {
-
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Print("$ ")
 
-		//read user input
 		command, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		command = strings.TrimSpace(command)
 
-		isEcho := strings.HasPrefix(command, "echo ")
-		if isEcho {
-			fmt.Println(command[5:])
+		command = strings.TrimSpace(command)
+		parts := strings.Fields(command)
+
+		if len(parts) == 0 {
 			continue
 		}
-		switch command {
+
+		implementedCommands := []string{"echo", "exit", "type"}
+
+		switch parts[0] {
+		case "echo":
+			fmt.Println(strings.Join(parts[1:], " "))
 		case "exit":
 			os.Exit(0)
-		}
+		case "type":
+			if len(parts) < 2 {
+				fmt.Println("Usage: type <command>")
+				continue
+			}
+			cmd := parts[1]
+			if contains(implementedCommands, cmd) {
+				fmt.Printf("%s is a shell builtin\n", cmd)
+			}
+			fmt.Println(cmd + ": command not found")
 
-		fmt.Println(command + ": command not found")
+		default:
+			fmt.Println(command + ": command not found")
+		}
 	}
+}
+
+func contains(implementedCommands []string, cmd string) bool {
+	for _, c := range implementedCommands {
+		if c == cmd {
+			return true
+		}
+	}
+	return false
 }
